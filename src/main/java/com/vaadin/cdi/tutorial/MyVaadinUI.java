da3c14d56077c23aedb77bdea076b9387e64170e
Juuso Valli <juuso@vaadin.com>
2014-10-17 11:28:05 +0300

    Prepare the UI for use with Vaadin CDI

--- src/main/java/com/vaadin/cdi/tutorial/MyVaadinUI.java
+++ src/main/java/com/vaadin/cdi/tutorial/MyVaadinUI.java
@@ -1,33 +1,25 @@
 package com.vaadin.cdi.tutorial;
 
-import javax.servlet.annotation.WebServlet;
-
 import com.vaadin.annotations.Theme;
-import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.cdi.CDIUI;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
-@Theme("mytheme")
+@Theme("valo")
+@CDIUI("")
 @SuppressWarnings("serial")
-public class MyVaadinUI extends UI
-{
-
-    @WebServlet(value = "/*", asyncSupported = true)
-    @VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class, widgetset = "com.vaadin.cdi.tutorial.AppWidgetSet")
-    public static class Servlet extends VaadinServlet {
-    }
+public class MyVaadinUI extends UI {
 
     @Override
     protected void init(VaadinRequest request) {
         final VerticalLayout layout = new VerticalLayout();
         layout.setMargin(true);
         setContent(layout);
-        
+
         Button button = new Button("Click Me");
         button.addClickListener(new Button.ClickListener() {
             public void buttonClick(ClickEvent event) {
